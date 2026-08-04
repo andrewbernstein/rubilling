96b11ef678e4d05b90364f65216dfc7315d0086a
Andrew Bernstein <andrewlbernstein@gmail.com>
2026-08-04 10:09:25 -0700

    dummy initialize and call inside CreateLineItemService

--- app/services/create_line_item_service.rb
+++ app/services/create_line_item_service.rb
@@ -1,5 +1,11 @@
 module Services
   class CreateLineItemService
+    def initialize
+      puts 'test initialize, please ignore'
+    end
 
+    def call
+      puts 'test call, please ignore'
+    end
   end
 end
