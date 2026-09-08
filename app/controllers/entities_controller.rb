5b4187ba2476fd895f21c887ca7e3631bb631eb9
Andrew Bernstein <andrewlbernstein@gmail.com>
2026-09-08 14:31:13 -0700

    Remove scan_js workflow step as this is an API, fix lint

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,17 +1,17 @@
 class EntitiesController < ApplicationController
   def index
-
+    # temp for now
   end
 
   def new
-
+    # temp for now
   end
 
   def create
-
+    # temp for now
   end
 
   def show
-
+    # temp for now
   end
 end
